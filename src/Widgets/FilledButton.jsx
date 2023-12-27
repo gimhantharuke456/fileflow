a76c786ca4d24f0a1ba834bbc7107a0b56275d98
gimhantharuke456 <gimhantharuke456@gmail.com>
2023-12-27 18:26:17 +0530

    ADD:splash screen done

---
+++ src/Widgets/FilledButton.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+
+const FilledWrapper = styled.div`
+  background-color: #4a9230; /* Filled color */
+  border: 1px solid #4a9230; /* Border color */
+  border-radius: 6px;
+  width: ${(props) => props.width || "auto"};
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const FilledText = styled.h5`
+  color: #ffffff; /* Text color */
+  text-align: center;
+  font-feature-settings: "clig" off, "liga" off;
+  font-family: Roboto;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 20px; /* 111.111% */
+  letter-spacing: 0.3px;
+`;
+
+const FilledButton = ({ onClick, width, text }) => {
+  return (
+    <FilledWrapper onClick={onClick} width={width}>
+      <FilledText>{text}</FilledText>
+    </FilledWrapper>
+  );
+};
+
+export default FilledButton;
